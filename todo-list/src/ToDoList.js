53eb97d84d0fff673147b000f66753d7e6c0cef2
Sunbearian <stigcollins@icloud.com>
2022-12-09 11:11:37 +0000

    Added get fetch request

--- todo-list/src/ToDoList.js
+++ todo-list/src/ToDoList.js
@@ -1,41 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 
-function ToDoList(){
-  const [todos, setTodos] = useState([])
-  
+function ToDoList() {
+	const [todos, setTodos] = useState([]);
 
-  function addTodo(e){
-    e.preventDefault()
-      const todo = {
-      text: e.target.elements.todo.value,
-      completed: false,
-    }
-    setTodos([...todos, todo])
-  }
+	useEffect(() => {
+		async function getTodos() {
+			const data = await fetch("http://localhost:3500/todos");
+			const response = await data.json();
 
-  function toggleTodo(index){
-    const newTodos = [...todos]
-    newTodos[index].completed = !newTodos[index].completed
-    setTodos(newTodos)
-  }
+			setTodos(response.payload);
+		}
+		getTodos();
+	}, []);
 
-  return(
-    <div className="ToDoList">
-    <h1 className="Title">ToDoList</h1>
-      <form onSubmit={addTodo}>
-        <input type="text" name="todo" />
-        <button type="submit">Add Todo</button>
-      </form>
-      <ul className='BulletList'>
-        {todos.map((todo, index) => (
-          <li key={index} onClick={()=> toggleTodo(index)}>{todo.text}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+	function addTodo(e) {
+		e.preventDefault();
+		const todo = {
+			task: e.target.elements.todo.value,
+			completed: false,
+		};
+		setTodos([...todos, todo]);
+	}
+
+	function toggleTodo(index) {
+		const newTodos = [...todos];
+		newTodos[index].completed = !newTodos[index].completed;
+		setTodos(newTodos);
+	}
+
+	return (
+		<div className="ToDoList">
+			<h1 className="Title">ToDoList</h1>
+			<form onSubmit={addTodo}>
+				<input type="text" name="todo" />
+				<button type="submit">Add Todo</button>
+			</form>
+			<ul className="BulletList">
+				{todos.map((todo, index) => (
+					<li key={index} onClick={() => toggleTodo(index)}>
+						{todo.task}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
-
-
-export default ToDoList
+export default ToDoList;
